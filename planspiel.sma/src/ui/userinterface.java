65a694b9cdad3bd8b8faa7069f76a6238eb6ae65
Lukas8342 <122049965+Lukas8342@users.noreply.github.com>
2023-01-13 17:37:08 +0100

    Update userinterface.java
    Updated UI

--- planspiel.sma/src/ui/userinterface.java
+++ planspiel.sma/src/ui/userinterface.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import domain.Rolle;
 import facade.gamecontroller;
 
 public class userinterface {
@@ -12,29 +13,99 @@ public class userinterface {
 	gamecontroller game = new gamecontroller();
 
 	public void startGame() {
-		print("Willkomen! ");
-		// asignRoles();
-		print("Sie können nun aus 5 verschiedenen Szenarien wählen");
-		System.out.println("_________________________________________________");
-		print("1. Daten im Darknet veröffentlicht");
+		game.createRoles();
+		boolean bool = true;
+		print("Willkommen !");
+		while (bool) {
+			System.out.println("");
+			print("1. Spiel starten ");
+			print("2. Einstellungen");
+			print("3. Beenden");
+			int wahl = Integer.parseInt(scan.nextLine());
+			switch (wahl) {
+			case 1:
+				System.out.println("____________________");
+				print("Spiel starten !");
+				asignRoles();
+				playScenario();
+				break;
+			case 2:
+				System.out.println("____________________");
+				print("Einstellungen");
+				settings();
+				break;
+			case 3:
+				System.out.println("_______________________");
+				print("Spiel beenden, tschÃ¼ss! ");
+				bool = false;
+			}
+		}
+
+	}
+
+	public void settings() {
+		print("1. Text-speed");
+		print("2. zurÃ¼ck");
+		int wahl = Integer.parseInt(scan.nextLine());
+		switch (wahl) {
+		case 1:
+			changeTextSpeed();
+			break;
+		case 2:
+			break;
+		}
+	}
+
+	public void changeTextSpeed() {
+		print("Text-Speed: " + textSpeed);
+		print("Geben sie den neuen TextSpeed ein");
+		textSpeed = Integer.parseInt(scan.nextLine());
+	}
+
+	public void playScenario() {
+		print("Sie kÃ¶nnen nun aus 5 verschiedenen Szenarien wÃ¤hlen");
+		System.out.println("=============================================");
+		print("1. Daten im Darknet verÃ¶ffentlicht");
+		System.out.println("---------------------------------------------");
 		print("2. Ein Zulieferer wurde gehackt");
-		print("3. Gestörte Windkraftanlagen");
+		System.out.println("---------------------------------------------");
+		print("3. GestÃ¶rte Windkraftanlagen");
+		System.out.println("---------------------------------------------");
 		print("4. Diebstahl von 30 Bitcoin");
+		System.out.println("---------------------------------------------");
 		print("5. Mitarbeiter verliert USB-Stick");
+		System.out.println("---------------------------------------------");
 		int wahl = Integer.parseInt(scan.nextLine()) - 1;
+
+		switch (wahl) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		}
+	}
+
+	public void playScenario3() {
 
 	}
 
 	public void asignRoles() {
-		String[] roles = { "Geschäftsleitung", "Informationssicherheitsbeauftragter", "Datenschutzbeauftragter",
+		String[] roles = { "GeschÃ¤ftsleitung", "Informationssicherheitsbeauftragter", "Datenschutzbeauftragter",
 				"Anwalt", "Head of Human Resources", "Head of Marketing" };
 		ArrayList<Integer> choosen = new ArrayList<Integer>();
 		ArrayList<String> roleList = new ArrayList<String>(Arrays.asList(roles));
-		print("Mit wie vielen Spielern möchten sie Spielen? (2mind - 6max)");
+		print("Mit wie vielen Spielern mÃ¶chten sie Spielen? (2mind - 6max)");
 		int anzahl = Integer.parseInt(scan.nextLine()) - 1;
+
 		for (int i = 0; i < anzahl; i++) {
 			System.out.println("");
-			System.out.println("Spieler " + (i + 1) + " wählen sie ihren Namen! ");
+			System.out.println("Spieler " + (i + 1) + " wÃ¤hlen sie ihren Namen! ");
 			print("Name: - ");
 			String name = scan.nextLine();
 
@@ -53,11 +124,12 @@ public class userinterface {
 	}
 
 	private void printRoles() {
-		print("Ihre gewählten Namen und Rollen: ");
-		String[][] arr = game.getRoleArray();
-		for (int i = 0; i < 6; i++) {
-			System.out.print((i + 1) + ". " + arr[i][0]);
-			System.out.println(" -" + arr[i][1]);
+		print("Ihre gewÃ¤hlten Namen und Rollen: ");
+		ArrayList<Rolle> allRoles = game.getRoles();
+		for (Rolle i : allRoles) {
+			if (i.getSpielername() != null) {
+				System.out.println(i.getId() + ": " + i.getName() + " - " + i.getSpielername());
+			}
 		}
 	}
 

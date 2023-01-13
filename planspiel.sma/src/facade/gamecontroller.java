d6229abd012f39e9f7f335b1059680ee2ba540dd
Lukas8342 <122049965+Lukas8342@users.noreply.github.com>
2023-01-13 17:37:30 +0100

    Update gamecontroller.java

--- planspiel.sma/src/facade/gamecontroller.java
+++ planspiel.sma/src/facade/gamecontroller.java
@@ -1,16 +1,23 @@
 package facade;
 
+import java.util.ArrayList;
+
 import domain.Roles;
+import domain.Rolle;
 
 public class gamecontroller {
 	Roles roles = new Roles();
 
 	public void asignRoles(String name, int i) {
-		roles.asign(name, i);
+		roles.asign(i, name);
 	}
 
-	public String[][] getRoleArray() {
-		return roles.getRoleArray();
+	public ArrayList<Rolle> getRoles() {
+		return roles.getRoles();
+	}
+
+	public void createRoles() {
+		roles.createAllRoles();
 	}
 
 }

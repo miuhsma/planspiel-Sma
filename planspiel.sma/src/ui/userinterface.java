bb1f091af382edfa98630dccd9f185d11084c978
Mike Ullrich <mike.ullrich@stud.hs-mannheim.de>
2023-02-08 23:41:52 +0100

    CleanerCode, Exceptionhandling für Eingaben

--- planspiel.sma/src/ui/userinterface.java
+++ planspiel.sma/src/ui/userinterface.java
@@ -2,14 +2,17 @@ package ui;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Predicate;
 
 import domain.Rolle;
 import facade.gamecontroller;
 
 public class userinterface {
 	Scanner scan = new Scanner(System.in);
-	private int textSpeed = 75;
+	private int textSpeed = 1;
+	private String message;
 	gamecontroller game = new gamecontroller();
 
 	public void startGame() {
@@ -18,16 +21,15 @@ public class userinterface {
 		print("Willkommen !");
 		while (bool) {
 			System.out.println("");
-			print("1. Spiel starten ");
-			print("2. Einstellungen");
-			print("3. Authoren");
-			print("4. Beenden");
-			int wahl = Integer.parseInt(scan.nextLine());
+			message = "1. Spiel starten\n2. Einstellungen\n3. Autoren\n4. Beenden";
+			print(message);
+			int wahl = validateInput(message, x -> x >= 1 && x <= 4);
+			
 			switch (wahl) {
 			case 1:
 				System.out.println("____________________");
 				print("Spiel starten !");
-				asignRoles();
+				assignRoles();
 				playScenario();
 				break;
 			case 2:
@@ -48,9 +50,10 @@ public class userinterface {
 	}
 
 	public void settings() {
-		print("1. Text-speed");
-		print("2. zurück");
-		int wahl = Integer.parseInt(scan.nextLine());
+		message = "1. Text-speed\n2. Zurück";
+		print(message);
+		
+		int wahl  = validateInput(message, x -> x == 1 || x == 2);
 		switch (wahl) {
 		case 1:
 			changeTextSpeed();
@@ -61,26 +64,31 @@ public class userinterface {
 	}
 
 	public void changeTextSpeed() {
+		message = "Geben sie den neuen TextSpeed ein (Zwischen 10 und 100)";
 		print("Text-Speed: " + textSpeed);
-		print("Geben sie den neuen TextSpeed ein");
-		textSpeed = Integer.parseInt(scan.nextLine());
+		print(message);
+		textSpeed = validateInput(message, x -> x >= 10 &&  x <= 100);
+		
 	}
 
 	public void playScenario() {
-		print("Sie können nun aus 5 verschiedenen Szenarien wählen");
-		System.out.println("=============================================");
-		print("1. Daten im Darknet veröffentlicht");
-		System.out.println("---------------------------------------------");
-		print("2. Ein Zulieferer wurde gehackt");
-		System.out.println("---------------------------------------------");
-		print("3. Gestörte Windkraftanlagen");
-		System.out.println("---------------------------------------------");
-		print("4. Diebstahl von 30 Bitcoin");
-		System.out.println("---------------------------------------------");
-		print("5. Mitarbeiter verliert USB-Stick");
-		System.out.println("---------------------------------------------");
-		int wahl = Integer.parseInt(scan.nextLine()) - 1;
+		message = "Sie können nun aus 5 verschiedenen Szenarien wählen\n" + 
+                "=============================================\n" +
+                "1. Daten im Darknet veröffentlicht\n" + 
+                "---------------------------------------------\n" +
+                "2. Ein Zulieferer wurde gehackt\n" + 
+                "---------------------------------------------\n" +
+                "3. Gestörte Windkraftanlagen\n" + 
+                "---------------------------------------------\n" +
+                "4. Diebstahl von 30 Bitcoin\n" + 
+                "---------------------------------------------\n" +
+                "5. Mitarbeiter verliert USB-Stick\n" + 
+                "---------------------------------------------\n";
 
+		print(message);
+
+		int wahl = validateInput(message, x -> (x > 0 && x < 5)) - 1;
+		 
 		switch (wahl) {
 		case 1:
 			break;
@@ -99,34 +107,58 @@ public class userinterface {
 
 	}
 
-	public void asignRoles() {
+	public void assignRoles() {
 		String[] roles = { "Geschäftsleitung", "Informationssicherheitsbeauftragter", "Datenschutzbeauftragter",
 				"Anwalt", "Head of Human Resources", "Head of Marketing" };
-		ArrayList<Integer> choosen = new ArrayList<Integer>();
-		ArrayList<String> roleList = new ArrayList<String>(Arrays.asList(roles));
-		print("Mit wie vielen Spielern möchten sie Spielen? (2mind - 6max)");
-		int anzahl = Integer.parseInt(scan.nextLine());
+		List<Integer> choosen = new ArrayList<>();
+		List<String> roleList = new ArrayList<>(Arrays.asList(roles));
+		message = "\nMit wie vielen Spielern möchten sie Spielen? (2 - 6)";
+		print(message);
+
+		int anzahl = validateInput(message, x -> x >= 2 && x <= 6 );
 
 		for (int i = 0; i < anzahl; i++) {
-			System.out.println("");
-			System.out.println("Spieler " + (i + 1) + " wählen sie ihren Namen! ");
-			print("Name: - ");
+			System.out.println("\nSpieler " + (i + 1) + ", wählen sie ihren Namen! ");
+			print("Name: ");
 			String name = scan.nextLine();
 
-			for (int n = 0; n < roleList.size(); n++) {
-				if (choosen.contains(n)) {
-					System.out.println((n + 1) + ". -" + " vergriffen"); // evtl Rot printen kein plan jo
-				} else {
-					System.out.println((n + 1) + ". -" + roles[n]);
-				}
-			}
-			int wahl = Integer.parseInt(scan.nextLine()) - 1;
+			message = "\nWählen Sie Ihre Rolle\n";
+			for (int n = 0; n < roleList.size(); n++) 
+				if (!choosen.contains(n))
+					message += (n + 1) + ". " + roles[n] + "\n";
+			int wahl = 0;
+			System.out.println(message);
+			wahl = validateInput(message, x -> (x > 0 && x <= 6) && !choosen.contains(x)) - 1;
+			
 			choosen.add(wahl);
 			game.asignRoles(name, wahl);
 		}
 		printRoles();
 	}
+	private int validateInput(String messageToRepeat, Predicate<Integer> condition) {
+		boolean valid = true;
 
+		int value = -1;
+
+		do {
+			valid = true;
+			try { 
+				System.out.print("\nEingabe: ");
+				value = Integer.parseInt(scan.nextLine());
+				if (!condition.test(value)) {
+					System.out.println("\nBitte die Auswahl beachten!\n" + messageToRepeat);
+					valid = false;
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Falsches Format!\n\n" + messageToRepeat);
+				valid = false;
+			}
+
+
+
+		} while (!valid);
+		return value;
+	}
 	private void printRoles() {
 		print("Ihre gewählten Namen und Rollen: ");
 		ArrayList<Rolle> allRoles = game.getRoles();
@@ -147,7 +179,6 @@ public class userinterface {
 			}
 
 		}
-		System.out.println("");
 	}
 
 	public void authoren() {
